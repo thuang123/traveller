22b46d2038f60677513854e0894f1cd6317ae4e7
Tony Huang <thuang_@hotmail.com>
2018-08-06 18:01:47 -0700

    Fix submit comment button

--- client/src/CommentForm.js
+++ client/src/CommentForm.js
@@ -3,26 +3,28 @@ import PropTypes from 'prop-types';
 
 const CommentForm = props => (
   <form onSubmit={props.submitComment}>
-    <input
-      type="text"
-      name="author"
-      placeholder="Your name…"
-      value={props.author}
-      onChange={props.handleChangeText}
-    />
-    <input
-      type="text"
-      name="text"
-      placeholder="Say something..."
-      value={props.text}
-      onChange={props.handleChangeText}
-    />
-    <button type="submit">Submit</button>
+  <input
+  type="text"
+  name="author"
+  placeholder="Your name…"
+  value={props.author}
+  onChange={props.handleChangeText}
+  />
+  <input
+  type="text"
+  name="text"
+  placeholder="Say something..."
+  value={props.text}
+  onChange={props.handleChangeText}
+  />
+  <button type="submit" onClick={props.handleSubmit}>
+  Submit
+  </button>
   </form>
 );
 
 CommentForm.propTypes = {
-  submitComment: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
   handleChangeText: PropTypes.func.isRequired,
   text: PropTypes.string,
   author: PropTypes.string,
