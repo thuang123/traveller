0bea4ac271100bb158c1c20df3ea7062fa8895be
Tony Huang <thuang_@hotmail.com>
2018-08-08 18:16:42 -0700

    Indentations

--- client/src/CommentBox.js
+++ client/src/CommentBox.js
@@ -11,7 +11,7 @@ class CommentBox extends Component {
       data: [],
       error: null,
       author: '',
-      text: ''
+      text: '',
     };
     this.pollInterval = null;
   }
@@ -28,114 +28,121 @@ class CommentBox extends Component {
     this.pollInterval = null;
   }
 
-  loadCommentsFromServer = () => {
-    // fetch returns a promise. If you are not familiar with promises, see
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
-    fetch('/api/comments/')
-    .then(data => data.json())
-    .then((res) => {
-      if (!res.success) this.setState({ error: res.error });
-      else this.setState({ data: res.data });
-    });
-  }
-
-  onChangeText = (e) => {
-    const newState = { ...this.state };
-    newState[e.target.name] = e.target.value;
-    this.setState(newState);
-  }
-
-  onUpdateComment = (id) => {
-    const oldComment = this.state.data.find(c => c._id === id);
-    if (!oldComment) return;
-    this.setState({
-      author: oldComment.author,
-      text: oldComment.text,
-      updateId: id
-    });
-  }
-
-  onDeleteComment = (id) => {
-    const i = this.state.data.findIndex(c => c._id === id);
-    const data = [
-      ...this.state.data.slice(0, i),
-      ...this.state.data.slice(i + 1),
-    ];
-    this.setState({ data });
-    fetch(`api/comments/${id}`, { method: 'DELETE' })
-    .then(res => res.json()).then((res) => {
-      if (!res.success) this.setState({ error: res.error });
-    });
-  }
-
-  submitComment = (e) => {
-    e.preventDefault();
-    const { author, text, updateId } = this.state;
-    if (!author || !text) return;
-    if (updateId) {
-      this.submitUpdatedComment();
-    } else {
-      this.submitNewComment();
+    loadCommentsFromServer = () => {
+      // fetch returns a promise. If you are not familiar with promises, see
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+      fetch('/api/comments/')
+        .then(data => data.json())
+        .then((res) => {
+          if (!res.success) this.setState({ error: res.error });
+          else this.setState({ data: res.data });
+        });
     }
-  }
 
-  submitNewComment = () => {
-    const { author, text } = this.state;
-    const data = [
-      ...this.state.data,
-      {
-        author,
-        text,
-        _id: Date.now().toString(),
-        updatedAt: new Date(),
-        createdAt: new Date()
-      },
-    ];
-    this.setState({ data });
-    fetch('/api/comments', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ author, text }),
-    }).then(res => res.json()).then((res) => {
-      if (!res.success) this.setState({ error: res.error.message || res.error });
-      else this.setState({ author: '', text: '', error: null });
-    });
-  }
+    onChangeText = (e) => {
+      const newState = { ...this.state };
+      newState[e.target.name] = e.target.value;
+      this.setState(newState);
+    }
 
-  submitUpdatedComment = () => {
-    const { author, text, updateId } = this.state;
-    fetch(`/api/comments/${updateId}`, {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ author, text }),
-    }).then(res => res.json()).then((res) => {
-      if (!res.success) this.setState({ error: res.error.message || res.error });
-      else this.setState({ author: '', text: '', updateId: null });
-    });
-  }
+    onUpdateComment = (id) => {
+      const oldComment = this.state.data.find(c => c._id === id);
+      if (!oldComment) return;
+      this.setState({
+        author: oldComment.author,
+        text: oldComment.text,
+        updateId: id,
+      });
+    }
 
-  render() {
-    return (
-      <div className="container">
-      <div className="comments">
-      <h2>Comments:</h2>
-      <CommentList
-      data={this.state.data}
-      handleDeleteComment={this.onDeleteComment}
-      handleUpdateComment={this.onUpdateComment}/>
-      </div>
-      <div className="form">
-      <CommentForm
-      author={this.state.author}
-      text={this.state.text}
-      handleChangeText={this.onChangeText}
-      handleSubmit={this.submitComment}
-      />
-      </div>
-      {this.state.error && <p>{this.state.error}</p>}
-      </div>
-    );
-  }
+    onDeleteComment = (id) => {
+      const i = this.state.data.findIndex(c => c._id === id);
+      const data = [
+        ...this.state.data.slice(0, i),
+        ...this.state.data.slice(i + 1),
+      ];
+      this.setState({ data });
+      fetch(`api/comments/${id}`, { method: 'DELETE' })
+        .then(res => res.json()).then((res) => {
+          if (!res.success) this.setState({ error: res.error });
+        });
+    }
+
+    submitComment = (e) => {
+      e.preventDefault();
+      const { author, text, updateId } = this.state;
+      if (!author || !text) return;
+      if (updateId) {
+        this.submitUpdatedComment();
+      } else {
+        this.submitNewComment();
+      }
+    }
+
+    submitNewComment = () => {
+      const { author, text } = this.state;
+      const data = [
+        ...this.state.data,
+        {
+          author,
+          text,
+          _id: Date.now().toString(),
+          updatedAt: new Date(),
+          createdAt: new Date(),
+        },
+      ];
+      this.setState({ data });
+      fetch('/api/comments', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ author, text }),
+      }).then(res => res.json()).then((res) => {
+        if (!res.success) this.setState({ error: res.error.message || res.error });
+        else this.setState({ author: '', text: '', error: null });
+      });
+    }
+
+    submitUpdatedComment = () => {
+      const { author, text, updateId } = this.state;
+      fetch(`/api/comments/${updateId}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ author, text }),
+      }).then(res => res.json()).then((res) => {
+        if (!res.success) this.setState({ error: res.error.message || res.error });
+        else this.setState({ author: '', text: '', updateId: null });
+      });
+    }
+
+    render() {
+      return (
+        <div className="container">
+          <div className="comments">
+            <h2>
+Comments:
+            </h2>
+            <CommentList
+              data={this.state.data}
+              handleDeleteComment={this.onDeleteComment}
+              handleUpdateComment={this.onUpdateComment}
+            />
+          </div>
+          <div className="form">
+            <CommentForm
+              author={this.state.author}
+              text={this.state.text}
+              handleChangeText={this.onChangeText}
+              handleSubmit={this.submitComment}
+            />
+          </div>
+          {this.state.error && (
+            <p>
+              {this.state.error}
+            </p>
+          )}
+        </div>
+      );
+    }
 }
 
 export default CommentBox;
--- client/src/CommentForm.js
+++ client/src/CommentForm.js
@@ -3,23 +3,23 @@ import PropTypes from 'prop-types';
 
 const CommentForm = props => (
   <form onSubmit={props.submitComment}>
-  <input
-  type="text"
-  name="author"
-  placeholder="Your name…"
-  value={props.author}
-  onChange={props.handleChangeText}
-  />
-  <input
-  type="text"
-  name="text"
-  placeholder="Say something..."
-  value={props.text}
-  onChange={props.handleChangeText}
-  />
-  <button type="submit" onClick={props.handleSubmit}>
-  Submit
-  </button>
+    <input
+      type="text"
+      name="author"
+      placeholder="Your name…"
+      value={props.author}
+      onChange={props.handleChangeText}
+    />
+    <input
+      type="text"
+      name="text"
+      placeholder="Say something..."
+      value={props.text}
+      onChange={props.handleChangeText}
+    />
+    <button type="submit" onClick={props.handleSubmit}>
+            Submit
+    </button>
   </form>
 );
 
